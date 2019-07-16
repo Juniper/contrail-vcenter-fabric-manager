ee536de8918db590b250cb24a8f37a45c8101596
Adrian Szczepański <adrian.szczepanski@codilime.com>
2019-07-16 14:55:18 +0200

    Replace watchdog with gevent.with_timeout
    Restart container when Zookeeper connection is lost
    
    Partial-Jira-Bug: CEM-5694
    Change-Id: I10d8345aadbfaf321bbec112dad91f10d767576c

--- cvfm/monitors.py
+++ cvfm/monitors.py
@@ -1,14 +1,57 @@
 import logging
 
+import gevent
+
+from cvfm.constants import EVENTS_TO_OBSERVE, WAIT_FOR_UPDATE_TIMEOUT
+
 logger = logging.getLogger(__name__)
 
 
 class VMwareMonitor(object):
-    def __init__(self, vmware_controller, update_set_queue):
+    def __init__(self, vmware_controller, vcenter_api_client):
         self._controller = vmware_controller
-        self._update_set_queue = update_set_queue
+        self._vcenter_api_client = vcenter_api_client
+        self._init()
 
-    def monitor(self):
+    def start(self):
+        self._controller.sync()
         while True:
-            update_set = self._update_set_queue.get()
-            self._controller.handle_update(update_set)
+            timeout = gevent.Timeout(WAIT_FOR_UPDATE_TIMEOUT * 2)
+            timeout.start()
+            try:
+                update_set = self._vcenter_api_client.wait_for_updates()
+                timeout.cancel()
+                if update_set:
+                    self._controller.handle_update(update_set)
+            except gevent.Timeout:
+                logger.error(
+                    "Waiting for vCenter updates timed out. Reconnecting..."
+                )
+                self._renew_vcenter_connection_retry()
+                self._init()
+                self._controller.sync()
+            finally:
+                timeout.cancel()
+
+    def _init(self):
+        event_history_collector = self._vcenter_api_client.create_event_history_collector(
+            EVENTS_TO_OBSERVE
+        )
+        self._vcenter_api_client.add_filter(
+            event_history_collector, ["latestPage"]
+        )
+        self._vcenter_api_client.make_wait_options(WAIT_FOR_UPDATE_TIMEOUT)
+        self._vcenter_api_client.wait_for_updates()
+
+    def _renew_vcenter_connection_retry(self):
+        i = 1
+        while True:
+            try:
+                self._vcenter_api_client.renew_connection()
+                logger.info("Successfully reconnected to vCenter")
+                break
+            except Exception:
+                logger.error("Error during renewing connection to vCenter")
+                gevent.sleep(2 * i)
+                logger.error("Retrying to renew connection to vCenter...")
+            i += 1
