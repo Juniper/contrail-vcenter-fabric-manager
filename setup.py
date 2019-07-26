32623c25a788017c79d8ff1b6ec77d901ac2f7ec
Adrian Szczepański <adrian.szczepanski@codilime.com>
2019-07-26 16:33:50 +0200

    Tox setup
    Partial-Jira-Bug: CEM-7552
    Change-Id: I9f705113912e00cce41345945f87ed9e49ca7ffe

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ def requirements(filename):
         lines = f.read().splitlines()
     return lines
 
+
 setup(
     name="contrail-vcenter-fabric-manager",
     version="0.1dev",
     packages=find_packages(),
-    package_data={'': ['*.html', '*.css', '*.xml', '*.yml']},
+    package_data={"": ["*.html", "*.css", "*.xml", "*.yml"]},
     zip_safe=False,
     long_description="Contrail vCenter Fabric Manager",
-    install_requires=requirements('requirements.txt'),
+    install_requires=requirements("requirements.txt"),
 )
