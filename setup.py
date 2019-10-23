878847c451f3f6d033b0194f4d1ca8340b2b3817
Adrian Szczepański <adrian.szczepanski@codilime.com>
2019-10-23 12:48:39 +0200

    Fix configparser import error
    Change-Id: Ie4d963c19e2b0f8535ed1f11906fc5d9f0916f1c
    Partial-Jira-Bug: CEM-9448

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from setuptools import find_packages, setup
 
 
-def requirements(filename):
-    with open(filename) as f:
-        lines = f.read().splitlines()
-    return lines
-
-
 setup(
     name="contrail-vcenter-fabric-manager",
     version="0.1dev",
@@ -14,10 +8,10 @@ setup(
     package_data={"": ["*.html", "*.css", "*.xml", "*.yml"]},
     zip_safe=False,
     long_description="Contrail vCenter Fabric Manager",
-    install_requires=requirements("requirements.txt"),
-    entry_points = {
-        'console_scripts' : [
-            'contrail-vcenter-fabric-manager = cvfm:server_main',
-        ],
+    install_requires=["six", "future"],
+    entry_points={
+        "console_scripts": [
+            "contrail-vcenter-fabric-manager = cvfm:server_main"
+        ]
     },
 )
